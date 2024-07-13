6584dc6c1b4932fdb42e31c3c1105f62bef7e493
moritz <moritz@fluidkey.com>
2024-07-13 20:36:40 +0200

    further ui work

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,6 +8,10 @@ import { useReadAaveUiPoolDataProviderGetReservesData, useReadAaveUiPoolDataProv
 import { formatUnits } from "viem";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default function Home() {
   const { data: walletClient } = useWalletClient();
@@ -76,39 +80,51 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center px-4 py-4 sm:px-16">
       <div className="flex justify-between items-center w-full sm:px-32">
-        <h1 className="text-2xl font-bold">fluid.loan</h1>
+        <h1 className={`text-2xl font-semibold ${raleway.className}`}>fluid.loan</h1>
         <ConnectButton />
       </div>
-      <div className="flex justify-center flex-wrap w-full mt-8 sm:mt-24 gap-4">
-        <Card className="sm:min-w-[40%] sm:w-[40%] min-w-[70%] relative flex flex-col justify-between">
-          <CardHeader>
-            <CardTitle className="text-4xl text-primary">💎 Hold ETH</CardTitle>
-            <CardDescription>
-              Keep the ETH upside and optimize taxable events.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="w-full flex flex-col justify-center items-center mt-5">
-            <p className="text-2xl sm:text-4xl text-primary">{ethCollateral} ETH</p>
-            <p className="text-lg text-primary mb-8">deposited</p>
-          </CardContent>
-        </Card>
-        <Card className="sm:min-w-[40%] sm:w-[40%] min-w-[70%] relative flex flex-col justify-between">
-          <CardHeader>
-            <CardTitle className="text-4xl text-primary">💵 Spend Dollars</CardTitle>
-            <CardDescription>
-              Borrow USDC against your ETH in one click to pay for your next
-              vacation, car, or any other expense.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="w-full flex flex-col justify-center items-center mt-5">
-            <p className="text-2xl sm:text-4xl text-primary">{usdcBorrowed} USDC</p>
-            <p className="text-lg text-primary mb-8">borrowed</p>
-          </CardContent>
-        </Card>
-      </div>
-      <div className="mt-4 sm:mt-24">
-        {depositAddress && address ? <p>Deposit address: {depositAddress}</p> :
-        <Button onClick={fetchSmartAccountClient}>Create deposit address</Button>}
+      <div className="flex flex-col justify-between items-center">
+        <div className="flex justify-center flex-wrap w-full mt-8 sm:mt-24 gap-4">
+          <Card className="sm:min-w-[40%] sm:w-[40%] min-w-[70%] relative flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-4xl text-primary">💎 Hold ETH</CardTitle>
+              <CardDescription>
+                Keep the ETH upside and optimize taxable events.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="w-full flex flex-col justify-center items-center mt-5">
+              <p className="text-2xl sm:text-4xl text-primary">{ethCollateral} ETH</p>
+              <p className="text-lg text-primary mb-8">deposited</p>
+            </CardContent>
+          </Card>
+          <Card className="sm:min-w-[40%] sm:w-[40%] min-w-[70%] relative flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-4xl text-primary">💵 Spend Dollars</CardTitle>
+              <CardDescription>
+                Borrow USDC against your ETH to pay for your next
+                vacation, car, or any other expense.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="w-full flex flex-col justify-center items-center mt-5">
+              <p className="text-2xl sm:text-4xl text-primary">{usdcBorrowed} USDC</p>
+              <p className="text-lg text-primary mb-8">borrowed</p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="flex flex-wrap justify-center w-full max-w-4xl mt-8 sm:mt-24 gap-2 items-center">
+          <Input type="text" placeholder="the address to send USDC to..." className="sm:max-w-[50%]" />
+          <div>
+            {depositAddress && address ? <p>Deposit address: {depositAddress}</p> :
+            <Button onClick={fetchSmartAccountClient} className="bg-[#4D8A8F] hover:bg-[#84B9BD]">Create deposit address →</Button>}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex justify-center w-full mt-16 mb-4 sm:mt-24 gap-4 sm:gap-12 flex-wrap">
+            <p className="text-2xl">✅ Zero clicks</p>
+            <p className="text-2xl">✅ Self-custodial</p>
+          </div>
+          <p className="max-w-2xl mt-2 text-muted-foreground">The fluid.loan module automatically borrows USDC on your behalf when ETH is deposited and automatically repays your loan when USDC is sent. The deposit address is a self-custodial Safe smart account controlled by the account connected to this page.</p>
+        </div>
       </div>
     </main>
   );
