c571e720bf48a4124d4879aab6473c0c766e0846
moritz <moritz@fluidkey.com>
2024-07-13 17:50:59 +0200

    work on frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 import { getSmartAccountClient, SafeSmartAccountClient } from "@/logic/permissionless";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { useAccount, useWalletClient } from "wagmi";
 import axios from "axios";
-import { useReadAaveUiPoolDataProvider, useReadAaveUiPoolDataProviderGetUserReservesData } from "@/generated";
+import { useReadAaveUiPoolDataProviderGetReservesData, useReadAaveUiPoolDataProviderGetUserReservesData } from "@/generated";
+import { formatUnits } from "viem";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { data: walletClient } = useWalletClient();
   const { address } = useAccount();
   const [smartAccountClient, setSmartAccountClient] = useState<SafeSmartAccountClient | undefined>();
-  const { data: readAaveBalances } = useReadAaveUiPoolDataProviderGetUserReservesData({
-    args: ["0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+  const { data: userReservesData } = useReadAaveUiPoolDataProviderGetUserReservesData({
+    args: ["0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb",
       "0x2748BDB378aaE458f322D17e0a0851c921D466E3"]
   });
+  const { data: reservesData } = useReadAaveUiPoolDataProviderGetReservesData({
+    args: ["0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb"]
+  });
+  const [ethCollateral, setEthCollateral] = useState<string>();
+  const [usdcBorrowed, setUsdcBorrowed] = useState<string>();
+  const [depositAddress, setDepositAddress] = useState<string>();
 
   const fetchSmartAccountClient = async () => {
     if (walletClient) {
@@ -21,22 +29,43 @@ export default function Home() {
       setSmartAccountClient(smartAccountClient);
     }
   }
-  useEffect(() => {
-    fetchSmartAccountClient();
-  }, [walletClient]);
 
   useEffect(() => {
-    console.log(readAaveBalances);
-  }, [readAaveBalances, address]);
+    const safeAddress = localStorage.getItem("safeAddress");
+    if (safeAddress) {
+      setDepositAddress(safeAddress);
+    }
+  }, []);
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 8,
+  });
+
+  useEffect(() => {
+    if (userReservesData && reservesData) {
+      const unformattedEthCollateral = userReservesData[0].find(balance => balance.underlyingAsset === "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1")?.scaledATokenBalance ?? BigInt(0);
+      const ethLiquidityIndex = reservesData[0].find(reserve => reserve.underlyingAsset === "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1")?.liquidityIndex ?? BigInt(0);
+      const finalEthCollateral = unformattedEthCollateral * ethLiquidityIndex;
+      const formattedEthCollateral = formatUnits(finalEthCollateral, 45);
+      const finalFormattedEthCollateral = formatter.format(Number(formattedEthCollateral));
+      setEthCollateral(finalFormattedEthCollateral);
+      const unformattedUsdcBorrowed = userReservesData[0].find(balance => balance.underlyingAsset === "0xaf88d065e77c8cC2239327C5EDb3A432268e5831")?.scaledVariableDebt ?? BigInt(0);
+      const usdcLiquidityIndex = reservesData[0].find(reserve => reserve.underlyingAsset === "0xaf88d065e77c8cC2239327C5EDb3A432268e5831")?.liquidityIndex ?? BigInt(0);
+      const finalUsdcBorrowed = unformattedUsdcBorrowed * usdcLiquidityIndex;
+      const formattedUsdcBorrowed = formatUnits(finalUsdcBorrowed ?? BigInt(0), 33);
+      const finalFormattedUsdcBorrowed = formatter.format(Number(formattedUsdcBorrowed));
+      setUsdcBorrowed(finalFormattedUsdcBorrowed);
+    }
+  }, [userReservesData, reservesData]);
 
   useEffect(() => {
     if (smartAccountClient) {
       axios.post("https://3ebks672jrgcw36vt4hvnyurvm0oyfuv.lambda-url.eu-west-1.on.aws/", {
-        body: {
-          "safeAddress": smartAccountClient.account.address,
-        }
+        "safeAddress": smartAccountClient.account.address,
       }).then((response) => {
-        console.log(response.data);
+        localStorage.setItem("safeAddress", smartAccountClient.account.address);
+        setDepositAddress(smartAccountClient.account.address);
       }).catch((error) => {
         console.error(error);
       });
@@ -44,10 +73,20 @@ export default function Home() {
   }, [smartAccountClient]);
     
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
-      <p>ETH deposited:</p>
-      <p>USDC borrowed:</p>
+    <main className="flex min-h-screen flex-col items-center px-4 py-4 sm:px-16">
+      <div className="flex justify-between items-center w-full">
+        <h1 className="text-2xl font-bold">fluid.loan</h1>
+        <ConnectButton />
+      </div>
+      <div className="flex justify-center flex-wrap w-full mt-24 gap-64">
+        <h2 className="text-4xl font-bold">Hold ETH</h2>
+        <h2 className="text-4xl font-bold">Pay with USDC</h2>
+      </div>
+      {depositAddress && address ? <p>Deposit address: {depositAddress}</p> :
+      <Button onClick={fetchSmartAccountClient}>Create deposit address</Button>}
+      
+      <p>ETH {ethCollateral} deposited</p>
+      <p>USDC {usdcBorrowed} borrowed</p>
     </main>
   );
 }
--- frontend/src/logic/permissionless.ts
+++ frontend/src/logic/permissionless.ts
@@ -3,7 +3,7 @@ import { SafeSmartAccount, signerToSafeSmartAccount, SmartAccountSigner } from '
 import { erc7579Actions, Erc7579Actions } from 'permissionless/actions/erc7579';
 import { createPimlicoBundlerClient, createPimlicoPaymasterClient } from 'permissionless/clients/pimlico';
 import { EntryPoint } from 'permissionless/types';
-import { Chain, createPublicClient, Hex, http, Transport, WalletClient } from 'viem';
+import { Chain, createPublicClient, Hex, http, Transport, WalletClient, encodePacked } from 'viem';
 import { arbitrum } from 'viem/chains';
 
 export type SafeSmartAccountClient = SmartAccountClient<
@@ -15,8 +15,8 @@ SafeSmartAccount<EntryPoint>
 Erc7579Actions<EntryPoint, SafeSmartAccount<EntryPoint>>
 
 const pimlicoUrl = `https://api.pimlico.io/v2/42161/rpc?apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
-const safe4337ModuleAddress = '0xa581c4A4DB7175302464fF3C06380BC3270b4037';
-const erc7569LaunchpadAddress = '0xEBe001b3D534B9B6E2500FB78E67a1A137f561CE';
+const safe4337ModuleAddress = '0x3Fdb5BC686e861480ef99A6E3FaAe03c0b9F32e2';
+const erc7579LaunchpadAddress = '0xEBe001b3D534B9B6E2500FB78E67a1A137f561CE';
 
 const publicClient = createPublicClient({
   transport: http('https://arb-mainnet.g.alchemy.com/v2/PQko-Hx_nZBkPbyYkEg8blONRN15Q3WA'),
@@ -40,9 +40,9 @@ export const getSmartAccountClient = async (walletClient: WalletClient) => {
     entryPoint: ENTRYPOINT_ADDRESS_V07,
     signer: signer,
     safeVersion: '1.4.1',
-    saltNonce: BigInt(120),
+    saltNonce: BigInt(121),
     safe4337ModuleAddress,
-    erc7569LaunchpadAddress,
+    erc7579LaunchpadAddress,
   });
 
   const smartAccountClient = createSmartAccountClient({
@@ -56,11 +56,18 @@ export const getSmartAccountClient = async (walletClient: WalletClient) => {
     },
   }).extend(erc7579Actions({ entryPoint: ENTRYPOINT_ADDRESS_V07 }));
 
-  console.log('Smart account client:', smartAccountClient.account.address);
-  const txHash = await smartAccountClient.sendTransaction({
-    to: "0x0000000000000000000000000000000000000000",
-    data: "0x",
-    value: BigInt(0)
-})
+  const install = await smartAccountClient.installModule({
+    type: 'executor',
+    address: '0x17E3d58BfcC08CD5f8B863b16743b21346b654aF',
+    context: encodePacked(['address'],['0x9Cb5433d5C5BDdc5C480103F06f03dB13b36b7C9']),
+    // nonce: BigInt(1),
+  });
+
+  console.log(install);
+//   const txHash = await smartAccountClient.sendTransaction({
+//     to: "0x0000000000000000000000000000000000000000",
+//     data: "0x",
+//     value: BigInt(0)
+// })
   return smartAccountClient as SafeSmartAccountClient;
 };
